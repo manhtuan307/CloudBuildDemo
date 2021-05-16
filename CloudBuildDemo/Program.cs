1aab6c59fe1a185361b8aa71115d315d21b78da3
manhtuan307 <manhtuan307@gmail.com>
2021-05-16 22:17:01 +0700

    Update

---
+++ CloudBuildDemo/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CloudBuildDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args) {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) {
+            var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+            var url = String.Concat("http://0.0.0.0:", port);
+
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => {
+                    webBuilder.UseStartup<Startup>().UseUrls(url);
+                });
+        }
+
+    }
+}
---
+++ Dockerfile
@@ -0,0 +1,18 @@
+﻿FROM mcr.microsoft.com/dotnet/sdk:5.0 AS build
+WORKDIR /app
+
+# Copy csproj and restore as distinct layers
+COPY *.sln .
+COPY CloudBuildDemo/*.csproj ./CloudBuildDemo/
+RUN dotnet restore
+
+# Copy everything else and build website
+COPY CloudBuildDemo/. ./CloudBuildDemo/
+WORKDIR /app/CloudBuildDemo
+RUN dotnet publish -c Release -o /Output
+
+# Final stage / image
+FROM mcr.microsoft.com/dotnet/aspnet:5.0
+WORKDIR /Output
+COPY --from=build /Output ./
+ENTRYPOINT ["dotnet", "CloudBuildDemo.dll"]
